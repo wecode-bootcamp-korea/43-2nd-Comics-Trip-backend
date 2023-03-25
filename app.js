e96497dd1a723e960103e0ed19a2d392b8d726e8
해동북 <hodozi@haedongbug-ui-MacBook-Pro.local>
2023-03-25 17:12:06 +0900

    [Modify] datasource 모듈화

--- app.js
+++ app.js
@@ -4,16 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const { DataSource } = require("typeorm");
-
-const dataSource = new DataSource({
-  type: process.env.DB_TYPE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+const dataSource = require("./api/models/dataSource");
 
 dataSource
   .initialize()
