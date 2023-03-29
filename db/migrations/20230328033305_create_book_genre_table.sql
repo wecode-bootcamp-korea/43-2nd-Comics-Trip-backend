f71b44054fe1d30eadfa3b00326d6522a7ec38e5
해동북 <hodozi@haedongbug-ui-MacBook-Pro.local>
2023-03-29 10:31:37 +0900

    [43기 최원식] 초기세팅 에러핸들링과 index파일 추가 -[43기 최원식] error.js 코드 수정

--- db/migrations/20230328033305_create_book_genre_table.sql
+++ db/migrations/20230328033305_create_book_genre_table.sql
@@ -1,8 +1,8 @@
 -- migrate:up
 CREATE TABLE book_genre (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  genre_id INT NOT NULL
-  book_id INT NOT NULL
+  genre_id INT NOT NULL,
+  book_id INT NOT NULL,
   CONSTRAINT book_genre_genre_id_fkey FOREIGN KEY (genre_id) REFERENCES genres(id),
   CONSTRAINT book_genre_book_id_fkey FOREIGN KEY (book_id) REFERENCES books(id)
 );
